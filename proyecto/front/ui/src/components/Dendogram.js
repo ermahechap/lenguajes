b396f405f2ebdab7f1546621d3f8a3cda3204012
jodorganistaca <jodorganistaca@unal.edu.co>
2020-01-07 16:37:31 -0500

    fixing dendogram component

--- proyecto/front/ui/src/components/Dendogram.js
+++ proyecto/front/ui/src/components/Dendogram.js
@@ -1,88 +1,69 @@
 import React from 'react';
 import { Group } from '@vx/group';
-import { Tree } from '@vx/hierarchy';
-import { LinkHorizontal } from '@vx/shape';
+import { Cluster } from '@vx/hierarchy';
+import { LinkVertical } from '@vx/shape';
 import { hierarchy } from 'd3-hierarchy';
 import { LinearGradient } from '@vx/gradient';
+import "../Styles/Dendogram.css"
 
-const peach = '#fd9b93';
-const pink = '#fe6e9e';
-const blue = '#03c0dc';
-const green = '#26deb0';
-const plum = '#71248e';
-const lightpurple = '#374469';
+const citrus = '#ddf163';
 const white = '#ffffff';
-const bg = '#272b4d';
+const green = '#79d259';
+const aqua = '#37ac8c';
+const merlinsbeard = '#f7f7f3';
+const bg = '#306c90';
 
-const tree = {
+const cluster = {
     "name": "T",
     "children": [{
         "name": "A",
         "children": [
             { "name": "A1" },
             { "name": "A2" },
-            { "name": "A3" },
             { "name": "C",
                 "children": [{
-                    "name": "C1",
-                }, {
-                    "name": "D",
-                    "children": [{
-                        "name": "D1"
-                    },{
-                        "name": "D2"
-                    },{
-                        "name": "D3"
-                    }]
-                }]
-            },
-        ]},
-        { "name": "Z" },
-        {
-            "name": "B",
-            "children": [
-                { "name": "B1"},
-                { "name": "B2"},
-                { "name": "B3"},
-            ]},
+                    "name": "C1"
+                }]},
+        ]}, {
+        "name": "B",
+        "children": [
+            { "name": "B1"},
+            { "name": "B2"},
+            { "name": "B3"},
+        ]},{
+        "name": "X",
+        "children": [{
+            "name": "Z"
+        }]}
     ],
 };
 
+
 function Node({ node }) {
-    const width = 40;
-    const height = 20;
-    const centerX = -width / 2;
-    const centerY = -height / 2;
     const isRoot = node.depth === 0;
     const isParent = !!node.children;
 
     if (isRoot) return <RootNode node={node} />;
-    if (isParent) return <ParentNode node={node} />;
 
     return (
-        <Group top={node.x} left={node.y}>
-            <rect
-                height={height}
-                width={width}
-                y={centerY}
-                x={centerX}
-                fill={bg}
-                stroke={green}
-                strokeWidth={1}
-                strokeDasharray={'2,2'}
-                strokeOpacity={0.6}
-                rx={10}
-                onClick={() => {
-                    alert(`clicked: ${JSON.stringify(node.data.name)}`);
-                }}
-            />
+        <Group top={node.y} left={node.x}>
+            {node.depth !== 0 && (
+                <circle
+                    r={12}
+                    fill={bg}
+                    stroke={isParent ? white : citrus}
+                    onClick={() => {
+                        alert(`clicked: ${JSON.stringify(node.data.name)}`);
+                    }}
+                />
+            )}
             <text
                 dy={'.33em'}
                 fontSize={9}
                 fontFamily="Arial"
                 textAnchor={'middle'}
-                fill={green}
                 style={{ pointerEvents: 'none' }}
+                fill={isParent ? white : citrus}
             >
                 {node.data.name}
             </text>
@@ -91,50 +72,21 @@ function Node({ node }) {
 }
 
 function RootNode({ node }) {
-    return (
-        <Group top={node.x} left={node.y}>
-            <circle r={12} fill="url('#lg')" />
-            <text
-                dy={'.33em'}
-                fontSize={9}
-                fontFamily="Arial"
-                textAnchor={'middle'}
-                style={{ pointerEvents: 'none' }}
-                fill={plum}
-            >
-                {node.data.name}
-            </text>
-        </Group>
-    );
-}
-
-function ParentNode({ node }) {
-    const width = 40;
-    const height = 20;
+    const width = 60;
+    const height = 40;
     const centerX = -width / 2;
     const centerY = -height / 2;
 
     return (
-        <Group top={node.x} left={node.y}>
-            <rect
-                height={height}
-                width={width}
-                y={centerY}
-                x={centerX}
-                fill={bg}
-                stroke={blue}
-                strokeWidth={1}
-                onClick={() => {
-                    alert(`clicked: ${JSON.stringify(node.data.name)}`);
-                }}
-            />
+        <Group top={node.y} left={node.x}>
+            <rect width={width} height={height} y={centerY} x={centerX} fill="url('#top')" />
             <text
                 dy={'.33em'}
                 fontSize={9}
                 fontFamily="Arial"
                 textAnchor={'middle'}
                 style={{ pointerEvents: 'none' }}
-                fill={white}
+                fill={bg}
             >
                 {node.data.name}
             </text>
@@ -143,50 +95,55 @@ function ParentNode({ node }) {
 }
 
 const Dendogram = ({
-                     width,
-                     height,
-                     margin = {
-                         top: 10,
-                         left: 30,
-                         right: 40,
-                         bottom: 80
-                     }
-                 }) => {
-    const data = hierarchy(tree);
-    const yMax = height - margin.top - margin.bottom;
+                       width = 500,
+                       height = 800,
+                       margin = {
+                           top: 40,
+                           left: 0,
+                           right: 0,
+                           bottom: 110
+                       }
+                   }) => {
+    const data = hierarchy(cluster);
     const xMax = width - margin.left - margin.right;
+    const yMax = height - margin.top - margin.bottom;
 
     return (
-        <svg width={width} height={height}>
-        <LinearGradient id="lg" from={peach} to={pink} />
-        <rect width={width} height={height} rx={14} fill={bg} />
-        <Tree root={data} size={[yMax, xMax]}>
-                {tree => {
-                    return (
-
-                        <Group top={margin.top} left={margin.left}>
-                            {tree.links().map((link, i) => {
-                                return (
-
-                                    <LinkHorizontal
-                                        key={`link-${i}`}
-                                        data={link}
-                                        stroke={lightpurple}
-                                        strokeWidth="1"
-                                        fill="none"
-                                    />
-                                );
-                            })}
-                            {tree.descendants().map((node, i) => {
-                                return <Node key={`node-${i}`} node={node} />;
-                            })}
-                        </Group>
-                    );
-                }}
-            </Tree>
-        </svg>
+        <div className="graph-container">
+            <svg className="graph">
+                <LinearGradient id="top" from={green} to={aqua} />
+                <rect width={width} height={height} rx={14} fill={bg} />
+                <Cluster root={data} size={[xMax, yMax]}>
+                    {cluster => {
+                        return (
+                            <Group top={margin.top} left={margin.left}>
+                                {cluster.links().map((link, i) => {
+                                    return (
+                                        <LinkVertical
+                                            key={`cluster-link-${i}`}
+                                            data={link}
+                                            stroke={merlinsbeard}
+                                            strokeWidth="1"
+                                            strokeOpacity={0.2}
+                                            fill="none"
+                                        />
+                                    );
+                                })}
+                                {cluster.descendants().map((node, i) => {
+                                    return <Node key={`cluster-node-${i}`} node={node} />;
+                                })}
+                            </Group>
+                        );
+                    }}
+                </Cluster>
+            </svg>
+            <h1 onClick={() => console.log(width)}>
+                click me
+            </h1>
+        </div>
 
     );
 };
+
 
 export default Dendogram
