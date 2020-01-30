2c3bef293f5e67729974183feee34f26e28bfcf7
jodorganistaca <jodorganistaca@unal.edu.co>
2020-01-30 09:13:28 -0500

    :fire: remove vx library and dependicies

--- proyecto/front/ui/src/App.js
+++ proyecto/front/ui/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Dendogram from  './components/Dendogram.js';
 import Code from "./components/Code";
 import { Row, Col } from 'antd';
 
@@ -13,9 +12,6 @@ function App() {
                 <Col span={24}>
                     <Code/>
                 </Col>
-                {/*<Col span={16}>*/}
-                {/*    <Dendogram/>*/}
-                {/*</Col>*/}
             </Row>
 
         </div>
