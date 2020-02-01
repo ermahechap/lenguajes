5bab24d9c0905ef8450e14b4fe04c62c21b87e06
Edwin M <ermahechap@unal.edu.co>
2020-02-01 11:57:07 -0500

    folder update, please pull

--- proyecto/tree.sh
+++
@@ -1,7 +0,0 @@
-shopt -s expand_aliases
-export CLASSPATH="/home/daniel/Downloads/downlands/antlr-4.7.1-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /home/daniel/Downloads/downlands/antlr-4.7.1-complete.jar'
-alias grun='java org.antlr.v4.gui.TestRig'
-cd out/production/proyecto
-
-grun Python3 file_input ../../../input/input.py -gui
