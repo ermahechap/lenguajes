83ca3b6e975cdbdc8ed09082f7f7ee0a22619286
jodorganistaca <jodorganistaca@unal.edu.co>
2020-02-03 01:32:47 -0500

    doesnt work

--- proyecto/front/ui/src/components/Code.js
+++ proyecto/front/ui/src/components/Code.js
@@ -2,6 +2,7 @@ import React, { useState,useContext,useEffect} from "react";
 import "../Styles/Code.css"
 import { Checkbox } from 'antd';
 import Context from "../GlobalState/context";
+import dataJson from "../input/input.json"
 
 const Code = (props) => {
 
@@ -25,14 +26,14 @@ const Code = (props) => {
     const CheckboxGroup = Checkbox.Group;
 
     const plainOptions = ['variable', 'list', 'function', 'dictionary', 'class', 'number', 'subscript', 'composed Element'];
-        /*variable: 'red',*/
-        /*list: 'green',*/
-        /*function: 'blue',*/
-        /*dictionary: 'purple',*/
-        /*class: 'orange',*/
-        /*number: 'aquamarine',*/
-        /*subscript: 'gold',*/
-        /*composed_element: 'lightblue'*/
+    /*variable: 'red',*/
+    /*list: 'green',*/
+    /*function: 'blue',*/
+    /*dictionary: 'purple',*/
+    /*class: 'orange',*/
+    /*number: 'aquamarine',*/
+    /*subscript: 'gold',*/
+    /*composed_element: 'lightblue'*/
     const defaultCheckedList = ['Apple', 'Orange'];
 
     const [checkboxOptions, setCheckboxOptions] = useState({
@@ -42,50 +43,31 @@ const Code = (props) => {
     });
 
 
-    // function onCheckAllChange(e) {
-    //     console.log(checkboxOptions);
-    //     setCheckboxOptions({
-    //         options: e.checked ? plainOptions : [],
-    //         indeterminate: false,
-    //         checkAll: e.checked
-    //     })
-    // };
-    //
-    // function onChange(checkboxOptions) {
-    //     console.log(checkboxOptions);
-    //     setCheckboxOptions({
-    //         ...checkboxOptions,
-    //         indeterminate: !checkboxOptions.length && checkboxOptions.length < plainOptions.length,
-    //         checkAll: checkboxOptions.length === plainOptions.length,
-    //     });
-    // }
-
-
-    const dataFromS = [
-        {"type": "ROOT", "id": 1, "parent_id":-1, "children_id": [], "from": [-1, -1], "to": [-1, -1]},
-        {"type": "variable", "id": 2, "parent_id":1, "children_id": [], "from": [1, 0], "to": [1, 0], "name": "A", "value_id": null},
-        {"type": "variable", "id": 3, "parent_id":1, "children_id": [], "from": [1, 2], "to": [1, 2], "name": "B", "value_id": null},
-        {"type": "variable", "id": 6, "parent_id":5, "children_id": [], "from": [1, 21], "to": [1, 21], "name": "D", "value_id": null},
-        {"type": "variable", "id": 7, "parent_id":5, "children_id": [], "from": [1, 23], "to": [1, 23], "name": "E", "value_id": null},
-        {"type": "variable", "id": 9, "parent_id":8, "children_id": [], "from": [1, 26], "to": [1, 26], "name": "F", "value_id": null},
-        {"type": "variable", "id": 10, "parent_id":8, "children_id": [], "from": [1, 28], "to": [1, 28], "name": "G", "value_id": null},
-        {"type": "subscript", "id": 12, "parent_id":11, "children_id": [], "from": [1, 32], "to": [1, 33]},
-        {"type": "variable", "id": 11, "parent_id":8, "children_id": [12], "from": [1, 30], "to": [1, 30], "name": "H", "value_id": null},
-        {"type": "list", "id": 8, "parent_id":5, "children_id": [9, 10, 11], "from": [1, 26], "to": [1, 35]},
-        {"type": "subscript", "id": 14, "parent_id":13, "children_id": [], "from": [1, 41], "to": [1, 42]},
-        {"type": "variable", "id": 13, "parent_id":5, "children_id": [14], "from": [1, 39], "to": [1, 39], "name": "I", "value_id": null},
-        {"type": "variable", "id": 15, "parent_id":5, "children_id": [], "from": [1, 46], "to": [1, 46], "name": "j", "value_id": null},
-        {"type": "subscript", "id": 5, "parent_id":4, "children_id": [6, 7, 8, 13, 15], "from": [1, 6], "to": [1, 48]},
-        {"type": "variable", "id": 4, "parent_id":1, "children_id": [5], "from": [1, 4], "to": [1, 4], "name": "C", "value_id": null},
-        {"type": "variable", "id": 17, "parent_id":16, "children_id": [], "from": [3, 1], "to": [3, 1], "name": "A", "value_id": null},
-        {"type": "variable", "id": 18, "parent_id":16, "children_id": [], "from": [3, 3], "to": [3, 3], "name": "B", "value_id": null},
-        {"type": "list", "id": 16, "parent_id":1, "children_id": [17, 18], "from": [3, 1], "to": [3, 5]},
-        {"type": "variable", "id": 20, "parent_id":19, "children_id": [], "from": [5, 1], "to": [5, 1], "name": "A", "value_id": null},
-        {"type": "variable", "id": 22, "parent_id":21, "children_id": [], "from": [5, 5], "to": [5, 5], "name": "A", "value_id": null},
-        {"type": "variable", "id": 23, "parent_id":21, "children_id": [], "from": [5, 7], "to": [5, 7], "name": "C", "value_id": null},
-        {"type": "list", "id": 21, "parent_id":19, "children_id": [22, 23], "from": [5, 5], "to": [5, 7]},
-        {"type": "list", "id": 19, "parent_id":1, "children_id": [20, 21], "from": [5, 1], "to": [5, 8]}
-    ];
+    // const dataFromS = [
+    //     {"type": "ROOT", "id": 1, "parent_id":-1, "children_id": [], "from": [-1, -1], "to": [-1, -1]},
+    //     {"type": "variable", "id": 2, "parent_id":1, "children_id": [], "from": [1, 0], "to": [1, 0], "name": "A", "value_id": null},
+    //     {"type": "variable", "id": 3, "parent_id":1, "children_id": [], "from": [1, 2], "to": [1, 2], "name": "B", "value_id": null},
+    //     {"type": "variable", "id": 6, "parent_id":5, "children_id": [], "from": [1, 21], "to": [1, 21], "name": "D", "value_id": null},
+    //     {"type": "variable", "id": 7, "parent_id":5, "children_id": [], "from": [1, 23], "to": [1, 23], "name": "E", "value_id": null},
+    //     {"type": "variable", "id": 9, "parent_id":8, "children_id": [], "from": [1, 26], "to": [1, 26], "name": "F", "value_id": null},
+    //     {"type": "variable", "id": 10, "parent_id":8, "children_id": [], "from": [1, 28], "to": [1, 28], "name": "G", "value_id": null},
+    //     {"type": "subscript", "id": 12, "parent_id":11, "children_id": [], "from": [1, 32], "to": [1, 33]},
+    //     {"type": "variable", "id": 11, "parent_id":8, "children_id": [12], "from": [1, 30], "to": [1, 30], "name": "H", "value_id": null},
+    //     {"type": "list", "id": 8, "parent_id":5, "children_id": [9, 10, 11], "from": [1, 26], "to": [1, 35]},
+    //     {"type": "subscript", "id": 14, "parent_id":13, "children_id": [], "from": [1, 41], "to": [1, 42]},
+    //     {"type": "variable", "id": 13, "parent_id":5, "children_id": [14], "from": [1, 39], "to": [1, 39], "name": "I", "value_id": null},
+    //     {"type": "variable", "id": 15, "parent_id":5, "children_id": [], "from": [1, 46], "to": [1, 46], "name": "j", "value_id": null},
+    //     {"type": "subscript", "id": 5, "parent_id":4, "children_id": [6, 7, 8, 13, 15], "from": [1, 6], "to": [1, 48]},
+    //     {"type": "variable", "id": 4, "parent_id":1, "children_id": [5], "from": [1, 4], "to": [1, 4], "name": "C", "value_id": null},
+    //     {"type": "variable", "id": 17, "parent_id":16, "children_id": [], "from": [3, 1], "to": [3, 1], "name": "A", "value_id": null},
+    //     {"type": "variable", "id": 18, "parent_id":16, "children_id": [], "from": [3, 3], "to": [3, 3], "name": "B", "value_id": null},
+    //     {"type": "list", "id": 16, "parent_id":1, "children_id": [17, 18], "from": [3, 1], "to": [3, 5]},
+    //     {"type": "variable", "id": 20, "parent_id":19, "children_id": [], "from": [5, 1], "to": [5, 1], "name": "A", "value_id": null},
+    //     {"type": "variable", "id": 22, "parent_id":21, "children_id": [], "from": [5, 5], "to": [5, 5], "name": "A", "value_id": null},
+    //     {"type": "variable", "id": 23, "parent_id":21, "children_id": [], "from": [5, 7], "to": [5, 7], "name": "C", "value_id": null},
+    //     {"type": "list", "id": 21, "parent_id":19, "children_id": [22, 23], "from": [5, 5], "to": [5, 7]},
+    //     {"type": "list", "id": 19, "parent_id":1, "children_id": [20, 21], "from": [5, 1], "to": [5, 8]}
+    // ];
 
 
     const codeFromServer =`<code>
@@ -113,46 +95,53 @@ A,B,C[5:10, 5, 5:10, D:E:[F,G,H[:10]], I[:5], j+5]
     let b = false;
 
     const fillObjects = (codeFromServer) =>{
-        for(let i = 1; i < dataFromS.length; i++) {
-            if((dataFromS[i].from[0]) === (dataFromS[i].to[0])){
-                if(!rows.has(dataFromS[i].from[0])){
-                    opens[dataFromS[i].from[0]] = {};
-                    closes[dataFromS[i].to[0]] = {};
-                    setByRows[dataFromS[i].to[0]] = new Set();
-                    rows.add((dataFromS[i].from[0]));
+        console.log(dataJson.data);
+        // console.log(dataFromS);
+        for(let i = 0; i < dataJson.data.length; i++) {
+            if(dataJson.data.parent_id === -1){
+
+            }else{
+                if((dataJson.data[i].from[0]) === (dataJson.data[i].to[0])){
+                    if(!rows.has(dataJson.data[i].from[0])){
+                        opens[dataJson.data[i].from[0]] = {};
+                        closes[dataJson.data[i].to[0]] = {};
+                        setByRows[dataJson.data[i].to[0]] = new Set();
+                        rows.add((dataJson.data[i].from[0]));
+                    }
+                }else {
+                    if(!rows.has(dataJson.data[i].from[0])){
+                        opens[dataJson.data[i].from[0]] = {};
+                        closes[dataJson.data[i].from[0]] = {};
+                        rows.add((dataJson.data[i].from[0]));
+                        setByRows[dataJson.data[i].from[0]] = new Set();
+                    }
+                    if(!rows.has(dataJson.data[i].to[0])){
+                        closes[dataJson.data[i].to[0]] = {};
+                        rows.add((dataJson.data[i].to[0]));
+                        setByRows[dataJson.data[i].to[0]] = new Set();
+                    }
+
                 }
-            }else {
-                if(!rows.has(dataFromS[i].from[0])){
-                    opens[dataFromS[i].from[0]] = {};
-                    closes[dataFromS[i].from[0]] = {};
-                    rows.add((dataFromS[i].from[0]));
-                    setByRows[dataFromS[i].from[0]] = new Set();
-                }
-                if(!rows.has(dataFromS[i].to[0])){
-                    closes[dataFromS[i].to[0]] = {};
-                    rows.add((dataFromS[i].to[0]));
-                    setByRows[dataFromS[i].to[0]] = new Set();
+                /*variable: 'red',*/
+                /*list: 'green',*/
+                /*function: 'blue',*/
+                /*dictionary: 'purple',*/
+                /*class: 'orange',*/
+                /*number: 'aquamarine',*/
+                /*subscript: 'gold',*/
+                /*composed_element: 'lightblue'*/
+                opens[dataJson.data[i].from[0]][dataJson.data[i].from[1]] = "<mark class="+ "w" + " type=" + dataJson.data[i].type + " id=" + dataJson.data[i].id +">";
+                closes[dataJson.data[i].to[0]][dataJson.data[i].to[1] + 1] = "</mark>";
+
+                if(!setByRows[dataJson.data[i].from[0]].has(dataJson.data[i].from[1])){
+                    setByRows[dataJson.data[i].from[0]].add(dataJson.data[i].from[1]);
                 }
 
-            }
-            /*variable: 'red',*/
-            /*list: 'green',*/
-            /*function: 'blue',*/
-            /*dictionary: 'purple',*/
-            /*class: 'orange',*/
-            /*number: 'aquamarine',*/
-            /*subscript: 'gold',*/
-            /*composed_element: 'lightblue'*/
-            opens[dataFromS[i].from[0]][dataFromS[i].from[1]] = "<mark class="+ "w" + " type=" + dataFromS[i].type + " id=" + dataFromS[i].id +">";
-            closes[dataFromS[i].to[0]][dataFromS[i].to[1] + 1] = "</mark>";
-
-            if(!setByRows[dataFromS[i].from[0]].has(dataFromS[i].from[1])){
-                setByRows[dataFromS[i].from[0]].add(dataFromS[i].from[1]);
+                if(!setByRows[dataJson.data[i].to[0]].has(dataJson.data[i].to[1]+1)){
+                    setByRows[dataJson.data[i].to[0]].add(dataJson.data[i].to[1].valueOf()+1);
+                }
             }
 
-            if(!setByRows[dataFromS[i].to[0]].has(dataFromS[i].to[1]+1)){
-                setByRows[dataFromS[i].to[0]].add(dataFromS[i].to[1].valueOf()+1);
-            }
         }
         b = true;
     };
@@ -283,8 +272,8 @@ A,B,C[5:10, 5, 5:10, D:E:[F,G,H[:10]], I[:5], j+5]
                 </figcaption>
                 <pre className="prettyprint"
                      dangerouslySetInnerHTML={{
-                    __html: code.str
-                }}>
+                         __html: code.str
+                     }}>
                 </pre>
             </figure>
         </div>
