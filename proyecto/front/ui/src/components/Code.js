9b4b7f632a29c81dbcab2b2a6d391cc61a75cd71
jodorganistaca <jodorganistaca@unal.edu.co>
2020-02-03 03:42:40 -0500

    god damn it

--- proyecto/front/ui/src/components/Code.js
+++ proyecto/front/ui/src/components/Code.js
@@ -2,7 +2,10 @@ import React, { useState,useContext,useEffect} from "react";
 import "../Styles/Code.css"
 import { Checkbox } from 'antd';
 import Context from "../GlobalState/context";
+import { Card } from 'antd';
 import dataJson from "../input/input.json"
+import { Row, Col } from 'antd';
+import Arbolito from "./Arbolito dime tu";
 
 const Code = (props) => {
 
@@ -24,8 +27,9 @@ const Code = (props) => {
     };
 
     const CheckboxGroup = Checkbox.Group;
+    const { Meta } = Card;
 
-    const plainOptions = ['variable', 'list', 'function', 'dictionary', 'class', 'number', 'subscript', 'composed Element'];
+    const plainOptions = ['variable', 'list', 'function', 'dictionary', 'class', 'number', 'subscript', 'composed Element', 'for_block'];
     /*variable: 'red',*/
     /*list: 'green',*/
     /*function: 'blue',*/
@@ -34,71 +38,62 @@ const Code = (props) => {
     /*number: 'aquamarine',*/
     /*subscript: 'gold',*/
     /*composed_element: 'lightblue'*/
-    const defaultCheckedList = ['Apple', 'Orange'];
-
-    const [checkboxOptions, setCheckboxOptions] = useState({
-        options: defaultCheckedList,
-        inderteminate: true,
-        checkAll: false
-    });
-
-
-    // const dataFromS = [
-    //     {"type": "ROOT", "id": 1, "parent_id":-1, "children_id": [], "from": [-1, -1], "to": [-1, -1]},
-    //     {"type": "variable", "id": 2, "parent_id":1, "children_id": [], "from": [1, 0], "to": [1, 0], "name": "A", "value_id": null},
-    //     {"type": "variable", "id": 3, "parent_id":1, "children_id": [], "from": [1, 2], "to": [1, 2], "name": "B", "value_id": null},
-    //     {"type": "variable", "id": 6, "parent_id":5, "children_id": [], "from": [1, 21], "to": [1, 21], "name": "D", "value_id": null},
-    //     {"type": "variable", "id": 7, "parent_id":5, "children_id": [], "from": [1, 23], "to": [1, 23], "name": "E", "value_id": null},
-    //     {"type": "variable", "id": 9, "parent_id":8, "children_id": [], "from": [1, 26], "to": [1, 26], "name": "F", "value_id": null},
-    //     {"type": "variable", "id": 10, "parent_id":8, "children_id": [], "from": [1, 28], "to": [1, 28], "name": "G", "value_id": null},
-    //     {"type": "subscript", "id": 12, "parent_id":11, "children_id": [], "from": [1, 32], "to": [1, 33]},
-    //     {"type": "variable", "id": 11, "parent_id":8, "children_id": [12], "from": [1, 30], "to": [1, 30], "name": "H", "value_id": null},
-    //     {"type": "list", "id": 8, "parent_id":5, "children_id": [9, 10, 11], "from": [1, 26], "to": [1, 35]},
-    //     {"type": "subscript", "id": 14, "parent_id":13, "children_id": [], "from": [1, 41], "to": [1, 42]},
-    //     {"type": "variable", "id": 13, "parent_id":5, "children_id": [14], "from": [1, 39], "to": [1, 39], "name": "I", "value_id": null},
-    //     {"type": "variable", "id": 15, "parent_id":5, "children_id": [], "from": [1, 46], "to": [1, 46], "name": "j", "value_id": null},
-    //     {"type": "subscript", "id": 5, "parent_id":4, "children_id": [6, 7, 8, 13, 15], "from": [1, 6], "to": [1, 48]},
-    //     {"type": "variable", "id": 4, "parent_id":1, "children_id": [5], "from": [1, 4], "to": [1, 4], "name": "C", "value_id": null},
-    //     {"type": "variable", "id": 17, "parent_id":16, "children_id": [], "from": [3, 1], "to": [3, 1], "name": "A", "value_id": null},
-    //     {"type": "variable", "id": 18, "parent_id":16, "children_id": [], "from": [3, 3], "to": [3, 3], "name": "B", "value_id": null},
-    //     {"type": "list", "id": 16, "parent_id":1, "children_id": [17, 18], "from": [3, 1], "to": [3, 5]},
-    //     {"type": "variable", "id": 20, "parent_id":19, "children_id": [], "from": [5, 1], "to": [5, 1], "name": "A", "value_id": null},
-    //     {"type": "variable", "id": 22, "parent_id":21, "children_id": [], "from": [5, 5], "to": [5, 5], "name": "A", "value_id": null},
-    //     {"type": "variable", "id": 23, "parent_id":21, "children_id": [], "from": [5, 7], "to": [5, 7], "name": "C", "value_id": null},
-    //     {"type": "list", "id": 21, "parent_id":19, "children_id": [22, 23], "from": [5, 5], "to": [5, 7]},
-    //     {"type": "list", "id": 19, "parent_id":1, "children_id": [20, 21], "from": [5, 1], "to": [5, 8]}
-    // ];
-
 
     const codeFromServer =`<code>
-class B:
-    def __init__(self):
-        pass
-    def f(self):
-        return 10;
-    def ff(self, z, *args):
-        return 10
-class A:
-    a = B
-    aa = B()
- A
-A()
-A.a
-A().a
-A.aa.f()
-A().aa.f()
-A().aa.ff(5,10)
- c = 10
-cc = c + 5
-cc = cc + 3
-zz = 1
-A = [1,2,3]
-A[:zz]
+# class B:
+#     def __init__(self):
+#         pass
+#     def f(self):
+#         return 10;
+#     def ff(self, z, *args):
+#         return 10
+# class A:
+#     a = B
+#     aa = B()
+#
+# A
+# A()
+# A.a
+# A().a
+# A.aa.f()
+# A().aa.f()
+# A().aa.ff(5,10)
+#
+# c = 10
+# cc = c + 5
+# cc = cc + 3
+# zz = 1
+# A = [1,2,3]
+# A[:zz]
+
+for i in range(10):
+    print(i)
+
+for i in [1,2,3]:
+    print(i)
+
+while True:
+    print(i)
+    break;
+
+A = 10
+
+if A == 10:
+    print(A)
+elif A == True:
+    print(A)
+else:
+    print(A)
 </code>`;
 
     const [code, setCode] = useState({
         str: codeFromServer
     });
+
+    const [cardInfo, setCardInfo] = useState({
+        title: "",
+        description: "",
+    })
 
     const rows = new Set();
 
@@ -248,6 +243,9 @@ A[:zz]
                         case "composed_element":
                             t = "e";
                             break;
+                        case "for_block":
+                            t = "for";
+                            break;
                     }
                     opens[r][i] = begin+t+end;
                     // console.log(begin, "w", end)
@@ -260,33 +258,52 @@ A[:zz]
     };
 
     return (
-        <div className="code-container">
-            <p id="example1-description">
-                This is the descriptive text before the code example:
-            </p>
-            <div className="checkbox-container">
+        <div>
+            <Row>
+                <Col span={12}>
+                    <div className="code-container">
+                        <p id="example1-description">
+                            This is the descriptive text before the code example:
+                        </p>
+                        <div className="checkbox-container">
+                            <CheckboxGroup
+                                options={plainOptions}
+                                onChange={(optionsSelected) => handleOptionsSelected(optionsSelected)}
+                                // value={checkboxOptions.options}
+                                // onChange={() => onChange(checkboxOptions.options)}
+                            />
+                        </div>
+                        <button onClick={()=> handleCodeFromServe(codeFromServer)}>
+                            Code
+                        </button>
+                        <figure>
+                            <figcaption id="example1-caption" onClick={() => setState()}>
+                                This is the caption
+                            </figcaption>
+                            <pre className="prettyprint"
+                                 dangerouslySetInnerHTML={{
+                                     __html: code.str
+                                 }}>
+                            </pre>
+                        </figure>
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div className="info-container">
+                        <Card style={{ width: 300, marginTop: 50 }}>
+                            <Meta
+                                title={cardInfo.title}
+                                description={cardInfo.description}
+                            />
+                        </Card>
+                    </div>
+                </Col>
 
-                <CheckboxGroup
-                    options={plainOptions}
-                    onChange={(optionsSelected) => handleOptionsSelected(optionsSelected)}
-                    // value={checkboxOptions.options}
-                    // onChange={() => onChange(checkboxOptions.options)}
-                />
-            </div>
-            <button onClick={()=> handleCodeFromServe(codeFromServer)}>
-                Code
-            </button>
-            <figure>
-                <figcaption id="example1-caption" onClick={() => setState()}>
-                    This is the caption
-                </figcaption>
-                <pre className="prettyprint"
-                     dangerouslySetInnerHTML={{
-                         __html: code.str
-                     }}>
-                </pre>
-            </figure>
+            </Row>
+
+
         </div>
+
     )
 }
 
